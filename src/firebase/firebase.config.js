1b0b82696147984355532f13fe71b74985b5d95a
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-05-02 16:02:16 +0600

    all the dependencies are installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwm_AejPbScVFUxmjggWtVGNgoQpL6v-Q",
+  authDomain: "taste-of-italy-532ce.firebaseapp.com",
+  projectId: "taste-of-italy-532ce",
+  storageBucket: "taste-of-italy-532ce.appspot.com",
+  messagingSenderId: "735088176142",
+  appId: "1:735088176142:web:3781eac424062770bce05f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
